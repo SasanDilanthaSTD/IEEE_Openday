1465586cb6e4dd85d9bbce8e17c3ed897b1d118e
Sasan Dilantha <sasandilantha@gmail.com>
2023-04-11 03:02:44 +0530

    2023-04-11: add newly Time counter in my design - 03:02

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ menu.addEventListener('click', function(){
 commitee.addEventListener('click', () =>{
     window.location.href = '../committee.html';
 });
+
+// time count
+const time_count = () => {
+    const count = new Date("April 20, 2023 08:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = count - now;
+
+    const sec = 1000;
+    const min = sec * 60;
+    const hour =  min * 60;
+    const day = hour * 24;
+
+    // calculatinga
+    const txtDay = Math.floor(gap / day);
+    const txtHour = Math.floor((gap % day) / hour);
+    const txtMin = Math.floor((gap % hour) / min);
+    const txtSec = Math.floor((gap % min) / sec);
+
+    document.querySelector(".day").innerText = txtDay;
+    document.querySelector(".hour").innerText = txtHour;
+    document.querySelector(".minute").innerText = txtMin;
+    document.querySelector(".second").innerText = txtSec;
+}
+
+setInterval(time_count, 1000);
